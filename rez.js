456d64f78f1912850ff26541bd3619bb0a3068f8
gergocs <56910451+gergocs@users.noreply.github.com>
2021-02-17 14:19:10 +0100

    Add files via upload

--- rez.js
+++ rez.js
@@ -1,53 +1,47 @@
-function addRow() {
+let webSocket = new WebSocket("ws://localhost:8080/json");
+
+webSocket.onmessage = function (event){
+    let msg = JSON.parse(event.data);
+    console.log(msg);
+}
+
+function addRow(){
     let empTab = document.getElementById('empTable');
 
-    let rowCnt = empTab.rows.length;   // table row count.
-    let tr = empTab.insertRow(rowCnt); // the table row.
+    let rowCnt = empTab.rows.length;
+    let tr = empTab.insertRow(rowCnt);
     tr = empTab.insertRow(rowCnt);
 
-    for (let c = 0; c < arrHead.length; c++) {
-        let td = document.createElement('td'); // table definition.
+    for(let c = 0; c < arrHead.length; c++){
+        let td = document.createElement('td');
         td = tr.insertCell(c);
 
-        if (c === 0) {      // the first column.
-            // add a button in every new row in the first column.
+        if(c === 0){
             let button = document.createElement('input');
 
-            // set input attributes.
             button.setAttribute('type', 'button');
             button.setAttribute('value', 'Remove');
-
-            // add button's 'onclick' event.
             button.setAttribute('onclick', 'removeRow(this)');
 
             td.appendChild(button);
-        }
-        else {
-            // 2nd, 3rd and 4th column, will have textbox.
+        }else{
             let ele = document.createElement('input');
             ele.setAttribute('type', 'text');
             ele.setAttribute('value', '');
-
             td.appendChild(ele);
         }
     }
 }
-function submit() {
+function submit(){
     let myTab = document.getElementById('empTable');
     let arrValues = [];
-
-    // loop through each row of the table.
-    for (let row = 1; row < myTab.rows.length - 1; row++) {
-        // loop through each cell in a row.
-        for (let c = 0; c < myTab.rows[row].cells.length; c++) {
+    for(let row = 1; row < myTab.rows.length - 1; row++){
+        for(let c = 0; c < myTab.rows[row].cells.length; c++){
             let element = myTab.rows.item(row).cells[c];
-            if (element.childNodes[0].getAttribute('type') === 'text') {
+            if(element.childNodes[0].getAttribute('type') === 'text'){
                 arrValues.push("'" + element.childNodes[0].value + "'");
             }
         }
     }
-
-    // The final output.
     document.getElementById('output').innerHTML = arrValues;
-    //console.log (arrValues);   // you can see the array values in your browsers console window. Thanks :-)
 }
